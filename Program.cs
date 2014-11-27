6ee645cfcf23f62e3ab1a6cef3bb123563ebf941
xandroid4net@gmail.com <xandroid4net@gmail.com>
2014-11-27 09:53:40 -0500

    MicrosoftOcrConsoleApp

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WindowsPreview.Media.Ocr;
+namespace ConsoleApplication4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            OcrEngine e = new OcrEngine(OcrLanguage.English);
+        }
+    }
+}
